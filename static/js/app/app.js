5c3600fa1d980eb057e90cf88f4d5d593a2425b6
Jason Solack <jsolack@gongos.com>
2018-04-20 10:54:07 -0400

    chartjs working

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -7,28 +7,18 @@ const app = new Vue({
       run_time: '',
       selected: [],
       platforms: [],
-      bar1_data: {
-          labels: [],
-          series: [],
-          chartPadding: 80,
-          labelOffset: 50,
-
-      },
-      pie1_data: {
-          labels: [],
-          series: [],
-          chartPadding: 80,
-          labelOffset: 50,
-      },
-      bar3_data: {
-          labels: [],
-          series: [],
-          chartPadding: 80,
-          labelOffset: 50,
-      },
+      chart1: null,
+      chart2: null,
+      chart3: null,
     },
   mounted: function () {
       // `this` points to the vm instance
+      var c1 = document.getElementById('chart1')
+      var c2 = document.getElementById('chart2')
+      var c3 = document.getElementById('chart3')
+      this.$data.chart1 = new Chart(c1, { type: 'bar' })
+      this.$data.chart2 = new Chart(c2, { type: 'line' })
+      this.$data.chart3 = new Chart(c3, { type: 'line' })
       this.get_platform()
       this.get_data()
   },
@@ -44,22 +34,32 @@ const app = new Vue({
         axios.get(GETDATA, {params: {selected: this.$data.selected}})
           .then(response => {
             var data = response.data
-            var bar1_data = this.$data.bar1_data
-            bar1_data.labels = data.bar1_data.labels
-            bar1_data.series = []
-            bar1_data.series.push(data.bar1_data.series.map((x) => {return parseFloat(x)}))
-            new Chartist.Bar('.ct-chart1', bar1_data)
+            var chart1_data = {
+                    datasets: [{
+                                data: data.bar1_data.series,
+                                label: 'Game Title'
+                              }],
+                    labels: data.bar1_data.labels
+                }
+            this.$data.chart1.data = chart1_data
+            this.$data.chart1.update()
 
-            var pie1_data = this.$data.pie1_data
-            pie1_data.labels = data.pie1_data.labels
-            pie1_data.series = []
-            pie1_data.series.push(data.pie1_data.series.map((x) => {return parseFloat(x)}))
-            new Chartist.Line('.ct-chart2', pie1_data)
+            var chart2_data = {
+                    datasets: [{
+                                data: data.pie1_data.series,
+                                label: 'Publisher'
+                              }],
+                    labels: data.pie1_data.labels
+                }
+            this.$data.chart2.data = chart2_data
+            this.$data.chart2.update()
 
-            var bar3_data = this.$data.bar3_data
-            bar3_data.labels = data.bar3_data.labels
-            bar3_data.series = data.bar3_data.series
-            new Chartist.Line('.ct-chart3', bar3_data)
+            var chart3_data = {
+                    datasets: data.bar3_data.series,
+                    labels: data.bar3_data.labels
+                }
+            this.$data.chart3.data = chart3_data
+            this.$data.chart3.update()
 
             this.$data.run_time = data.run_time
           })
