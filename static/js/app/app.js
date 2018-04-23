686268870913ba43c951ebf90a575d51c35feba4
Jason Solack <jsolack@gongos.com>
2018-04-23 12:26:22 -0400

    revampin

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -1,68 +1,91 @@
-Vue.options.delimiters
-const app = new Vue({
-    delimiters: ['{!',  '!}'],
-  // create your Vue Object
-  el: '#main',
-  data: {
-      run_time: '',
-      selected: [],
-      platforms: [],
-      bar1_data: {
-          labels: [],
-          series: [],
-          chartPadding: 80,
-          labelOffset: 50,
+import Chart from './components/area_chart.js'
 
-      },
-      pie1_data: {
-          labels: [],
-          series: [],
-          chartPadding: 80,
-          labelOffset: 50,
-      },
-      bar3_data: {
-          labels: [],
-          series: [],
-          chartPadding: 80,
-          labelOffset: 50,
-      },
-    },
-  mounted: function () {
-      // `this` points to the vm instance
-      this.get_platform()
-      this.get_data()
-  },
-  methods: {
-      get_platform: function () {
-          axios.get(GETPLATFORM)
-          .then (response => {
-                this.$data.platforms = response.data
-                this.$data.selected = response.data
-          })
-      },
-      get_data: function () {
-        axios.get(GETDATA, {params: {selected: this.$data.selected}})
-          .then(response => {
-            var data = response.data
-            var bar1_data = this.$data.bar1_data
-            bar1_data.labels = data.bar1_data.labels
-            bar1_data.series = []
-            bar1_data.series.push(data.bar1_data.series.map((x) => {return parseFloat(x)}))
-            new Chartist.Bar('.ct-chart1', bar1_data)
+Vue.use(VueHighcharts)
+Vue.options.delimiters = ['{%', '%}']
 
-            var pie1_data = this.$data.pie1_data
-            pie1_data.labels = data.pie1_data.labels
-            pie1_data.series = []
-            pie1_data.series.push(data.pie1_data.series.map((x) => {return parseFloat(x)}))
-            new Chartist.Line('.ct-chart2', pie1_data)
 
-            var bar3_data = this.$data.bar3_data
-            bar3_data.labels = data.bar3_data.labels
-            bar3_data.series = data.bar3_data.series
-            new Chartist.Line('.ct-chart3', bar3_data)
+new Vue({
+	el: '#main',
+	data: {
+		age: 55,
+		currentPensionValue: 22000,
+		retireAge: 87
+	},
+    computed: {
+        data() { return (this.currentPensionValue) / (this.retireAge - this.age) },
+        steven() { return this.data * 1.4 },
+        agePotValue() {
+            var vm = this;
+            var agePotValue = [[vm.age, (vm.data)], [vm.retireAge, vm.currentPensionValue]];
 
-            this.$data.run_time = data.run_time
-          })
+            return agePotValue;
         }
-    }
+	},
+	components: { Chart }
 })
+// const app = new Vue({
+//   // create your Vue Object
+//   el: '#main',
+//   data: {
+//       run_time: '',
+//       selected: [],
+//       platforms: [],
+//       bar1_data: {
+//           labels: [],
+//           series: [],
+//           chartPadding: 80,
+//           labelOffset: 50,
+
+//       },
+//       pie1_data: {
+//           labels: [],
+//           series: [],
+//           chartPadding: 80,
+//           labelOffset: 50,
+//       },
+//       bar3_data: {
+//           labels: [],
+//           series: [],
+//           chartPadding: 80,
+//           labelOffset: 50,
+//       },
+//     },
+//   mounted: function () {
+//       // `this` points to the vm instance
+//       this.get_platform()
+//       this.get_data()
+//   },
+//   methods: {
+//       get_platform: function () {
+//           axios.get(GETPLATFORM)
+//           .then (response => {
+//                 this.$data.platforms = response.data
+//                 this.$data.selected = response.data
+//           })
+//       },
+//       get_data: function () {
+//         axios.get(GETDATA, {params: {selected: this.$data.selected}})
+//           .then(response => {
+//             var data = response.data
+//             var bar1_data = this.$data.bar1_data
+//             bar1_data.labels = data.bar1_data.labels
+//             bar1_data.series = []
+//             bar1_data.series.push(data.bar1_data.series.map((x) => {return parseFloat(x)}))
+//             new Chartist.Bar('.ct-chart1', bar1_data)
+
+//             var pie1_data = this.$data.pie1_data
+//             pie1_data.labels = data.pie1_data.labels
+//             pie1_data.series = []
+//             pie1_data.series.push(data.pie1_data.series.map((x) => {return parseFloat(x)}))
+//             new Chartist.Line('.ct-chart2', pie1_data)
+
+//             var bar3_data = this.$data.bar3_data
+//             bar3_data.labels = data.bar3_data.labels
+//             bar3_data.series = data.bar3_data.series
+//             new Chartist.Line('.ct-chart3', bar3_data)
+
+//             this.$data.run_time = data.run_time
+//           })
+//         }
+//     }
+// })
---
+++ static/js/app/components/area_chart.js
@@ -0,0 +1,70 @@
+const Chart = Vue.component('Chart', {
+    props: ['data', 'steven', 'agePotValue'],
+    name: 'Chart',
+	template: `
+	<div>
+		<p>{{ data }}</p>
+		<h1>{{ steven }}</h1>
+		<h1>{{ agePotValue }}</h1>
+		<div id="thechart"></div>
+	</div>
+	`,
+  data(){
+  	return{
+  		chart:undefined
+  	}
+  },
+  methods:{
+  	redraw(){
+    	this.chart.series[0].setData(this.agePotValue,true);
+    }
+  },
+  watch:{
+  	data(){this.redraw()},
+    steven(){this.redraw()},
+    agePotValue(){this.redraw()},
+  },
+	mounted() {
+		var highchartsOptions = {
+			chart: {
+				type: 'area',
+				renderTo: 'thechart'
+			},
+			credits: {
+				enabled: false
+			},
+			tooltip: {
+				enabled: false
+			},
+			title: {
+				text: ''
+			},
+			xAxis: {
+				allowDecimals: false,
+				title: {
+					text: 'Age'
+				}
+			},
+			yAxis: {
+				title: {
+				  text: 'Pot Value'
+				},
+				labels: {
+					formatter: function () {
+				    return '$' + this.value / 1000 + 'k';
+				  }
+				},
+				opposite: true,
+			},
+			plotOptions: {},
+			series: [{
+                name: '',
+                data: this.agePotValue
+            }],
+			credits: false
+    	}
+        this.chart = new Highcharts.chart(highchartsOptions)
+	}
+});
+
+export default Chart
